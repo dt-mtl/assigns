b87baebad73ff9df2d8193f7fdfc2b8eb11e227c
dt-mtl <57647395+dt-mtl@users.noreply.github.com>
2020-01-28 15:55:38 -0500

    Add files via upload

--- Assign1.java
+++ Assign1.java
@@ -5,14 +5,57 @@
 // -----------------------------------------------------
 import java.util.Scanner;
 public class Assign1 {
-
+	final String pass="c249"; //password 
 	public static void main(String[] args) {
 		
-		Scanner input=new Scanner(System.in);
+		Scanner input=new Scanner(System.in); 
+		//required variables
+		int numAppli; // number of appliances the store can hold
+		int option;
+		int attempts=0;
 		
-		
-		Appliance holla= new Appliance();
+		//welcome message
 		welcome();
+		System.out.print("How many appliances will your store hold? ");
+		numAppli=input.nextInt();
+		Appliance[] inventory= new Appliance[numAppli]; //appliance inventory array
+		System.out.println();
+		
+		do {
+		
+			prompt(); // first prompt
+			option=input.nextInt();
+			System.out.println();
+			switch(option) {
+				case 1:
+					System.out.println("\noption 1");
+					System.out.println();
+					break;
+				case 2:
+					System.out.println("option 2");
+					System.out.println();
+					break;
+				case 3:
+					System.out.println("All appliances of this exact same brand");
+					System.out.println();
+					break;
+				case 4:
+					System.out.println("Find a cheaper appliance than this price");
+					System.out.println();
+					break;
+				case 5:
+					System.out.println("Thank you for using the appliance directory, Have a great day :D");
+					System.out.println();
+					break;
+				default:
+					System.out.println("Please enter a valid option between 1-5");
+					System.out.println();
+			}
+			
+		}while(attempts<=12 && option!= 5);
+		
+		
+	
 	}
 	
 	//banner display message program
@@ -20,5 +63,31 @@ public class Assign1 {
 		System.out.println("************************************************************************************************************"
 						 + "\n*                                WELCOME TO THE APPLIANCE DIRECTORY                                        *" 
 				         + "\n************************************************************************************************************");
+	}
+	//options prompt
+	public static void prompt() {
+		System.out.print("What do you want to do?\r\n" + 
+				"\t1. Enter new appliances (password required)\r\n" + 
+				"\t2. Change information of an appliance (password required)\r\n" + 
+				"\t3. Display all appliances by a specific brand\r\n" + 
+				"\t4. Display all appliances under a certain a price.\r\n" + 
+				"\t5. Quit\r\n" + 
+				"Please enter your choice > ");
+	}
+	//information menu
+	public static void appliInfo() {
+		System.out.print("What information would you like to change?\r\n" + 
+				"\t1. brand\r\n" + 
+				"\t2. type\r\n" + 
+				"\t3. price\r\n" + 
+				"\t4. Quit\r\n" + 
+				"Enter your choice > ");
+	}
+	//password validity check
+	public boolean passCheck(String word ) {
+		if (word.equals(pass))
+			return true;
+		else
+			return false;
 	}
 }
